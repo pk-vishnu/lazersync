5a5119680474dd8e86ad4165fd13c51820aaec8a
Vishnu P K <vishnuvpk05@gmail.com>
2025-02-18 19:52:51 +0530

    fix: load dll before import to prevent dependency errors

--- visualiser_normalised/device.py
+++ visualiser_normalised/device.py
@@ -1,8 +1,18 @@
 import ctypes
-import os
-import hid
-import sys
 import platform
+import sys
+
+try:
+    if platform.system() == "Windows":
+        ctypes.CDLL('../utils/hidapi.dll')
+    elif platform.system() == "Darwin":
+        ctypes.CDLL("libhidapi.dylib")  # Update the path to your installation if hidapi 
+except OSError as e:  # Handle missing or incorrect DLL path
+    print(f"Error loading HIDAPI library: {e}")
+    sys.exit(1)  # Exit if the HIDAPI library fails to load
+
+import hid
+import os
 
 # Device Info
 VENDOR_ID = 0x342D
@@ -14,26 +24,8 @@ class HIDDevice:
     def __init__(self):
         self.device = self.initialize_device()
 
-    def load_hidapi_library(self):
-        """Load the HIDAPI library based on the operating system."""
-        try:
-            if platform.system() == "Windows":
-                # Load hidapi.dll on Windows
-                ctypes.CDLL("hidapi.dll")
-            elif platform.system() == "Darwin":  # macOS
-                # Load libhidapi.dylib on macOS
-                ctypes.CDLL("libhidapi.dylib")
-            else:
-                raise Exception("Unsupported operating system")
-        except Exception as e:
-            print(f"Failed to load HIDAPI library: {e}")
-            sys.exit(1)
-
     def initialize_device(self):
         """Initializes the HID device and returns the device handle."""
-        # Load the HIDAPI library
-        self.load_hidapi_library()
-
         # Enumerate devices
         devices = hid.enumerate(VENDOR_ID, PRODUCT_ID)
         if not devices:
@@ -72,13 +64,3 @@ class HIDDevice:
     def cleanup(self):
         """Close the HID device."""
         self.device.close()
-
-# Only run this test if the module is executed directly.
-if __name__ == '__main__':
-    device = HIDDevice()
-    print("HID device initialized successfully!")
-    try:
-        data = device.device.read(64, timeout=500)
-        print("Data read from device:", data)
-    except Exception as e:
-        print("Error reading from device:", e)
